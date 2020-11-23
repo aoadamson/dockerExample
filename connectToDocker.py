657da81fb9e60f9ac786591ae530ed244f4fba14
austinadamson <aoadamson@gmail.com>
2020-11-23 00:18:29 -0600

    Dockerfile works now for auto generating the data.

--- connectToDocker.py
+++ connectToDocker.py
@@ -1,27 +1,27 @@
 from psycopg2 import connect
 
-table_create = """CREATE TABLE IF NOT EXISTS films (
-    title       varchar(40)
-);"""
+# table_create = """CREATE TABLE IF NOT EXISTS films (
+#     title       varchar(40)
+# );"""
+#
+# table_insert = "INSERT INTO films VALUES ('Logan');"
 
-table_insert = "INSERT INTO films VALUES ('Logan');"
-
-table_select = "SELECT distinct * FROM films;"
+table_select = "SELECT * FROM films;"
 
 # declare connection instance
 conn = connect(
     host="localhost",
-    database="postgres",
-    user="unicorn_user",
-    password="magical_password")
+    database="testdb",
+    user="postgres",
+    password="postgres")
 
 # declare a cursor object from the connection
 cursor = conn.cursor()
 
 # execute an SQL statement using the psycopg2 cursor object
-cursor.execute(f"{table_create}")
-cursor.execute(f"{table_insert}")
-conn.commit()
+# cursor.execute(f"{table_create}")
+# cursor.execute(f"{table_insert}")
+# conn.commit()
 cursor.execute(f"{table_select}")
 
 records = cursor.fetchall()
---
+++ test.sql
@@ -0,0 +1,2 @@
+CREATE TABLE IF NOT EXISTS films (title varchar(40));
+INSERT INTO films VALUES ('Logan');
